3bd6bc452aae52a1e4c1885242397a064e059926
vjosaukella <vjosa.ukella@vtsd.com>
2021-10-20 13:11:23 -0400

    wooo

---
+++ CatchATurtle.py
@@ -0,0 +1,15 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+
+
+#-----game configuration----
+
+
+#-----initialize turtle-----
+
+
+#-----game functions--------
+
+
+#-----events----------------
